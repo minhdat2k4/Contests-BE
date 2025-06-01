01e6a70d436ee19001eecee8acabfd3682ea9e61
minhdat204 <huynhminhdat1506@gmail.com>
2025-06-01 07:07:21 +0700

    feat: Implement user seeding functionality with admin account creation

---
+++ prisma/seeders/users.seed.ts
@@ -0,0 +1,28 @@
+import { hash } from "bcrypt";
+import { SeedParams } from "./seed.interface";
+
+export default async function seedUsers({ prisma, logger, env = process.env } : SeedParams) {
+    try {
+        await prisma.user.deleteMany();
+        const username = env.UsernameAdmin || "admin";
+        const password = env.PasswordAdmin || "admin123";
+        const email = env.EmailAdmin || "admin@example.com";
+        const adminPassword = await hash(password, 10);
+        const adminUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: adminPassword,
+                role: "Admin",
+            },
+        });
+
+        logger.info(
+            `Tạo tài khoản admin thành công: ${adminUser.username} với email ${adminUser.email}`
+        );
+        return adminUser;
+    } catch (error) {
+        logger.error("Tạo tài khoản admin thất bại:", error);
+        throw error;
+    }
+}

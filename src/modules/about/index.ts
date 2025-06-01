04e415efcfde379f76a58265b5126954a8a9ba2c
minhdat204 <huynhminhdat1506@gmail.com>
2025-06-01 19:10:37 +0700

    feat: Add index file to export About module components

---
+++ src/modules/about/index.ts
@@ -0,0 +1,5 @@
+// Export all About module components
+export { default as AboutService } from './about.service';
+export { default as AboutController } from './about.controller';
+export { aboutRouter } from './about.routes';
+export * from './about.schema';

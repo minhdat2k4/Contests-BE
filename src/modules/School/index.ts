10dc75d617270c20450e6ea3ccfd59eb07ccd47c
longphan2k4 <ptlong2k4@gmail.com>
2025-06-26 22:54:29 +0700

    tạm đổi tên thư mục School thành temp để git ghi nhận

--- src/modules/School/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as SchoolController } from "./school.controller";
-export { default as SchoolService } from "./school.service";
-export { schoolRouter } from "./school.routes";
-export * from "./school.schema";

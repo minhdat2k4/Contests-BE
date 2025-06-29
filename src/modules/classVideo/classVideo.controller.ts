a0f448d9c8676d730d59d88ee467b45b0a715066
longphan2k4 <ptlong2k4@gmail.com>
2025-06-29 12:34:41 +0700

    fix-1

--- src/modules/classVideo/classVideo.controller.ts
+++ src/modules/classVideo/classVideo.controller.ts
@@ -79,7 +79,7 @@ export default class ClassVideoController {
 
       if (!req.file) throw new Error(`Vui lòng upload file`);
       const file = req.file;
-      const folderPath = path.resolve(process.cwd(), "uploads", "ClassVideo");
+      const folderPath = path.resolve(__dirname, "uploads", "ClassVideo");
       await ensureFolderExists(folderPath);
       const info = prepareFileInfoCustom(file, folderPath);
       const data = {
